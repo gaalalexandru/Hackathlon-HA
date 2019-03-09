4f675decfcaf1297df7a638dac19bed63fe858bf
florealucian <florea.razvanlucian@gmail.com>
2019-03-09 13:33:11 +0200

    I2C_1try

--- Hackathlon-HA/Hackathlon-HA/Debug/Hackathlon-HA.d
+++ Hackathlon-HA/Hackathlon-HA/Debug/Hackathlon-HA.d
@@ -12,7 +12,10 @@ Hackathlon-HA.d Hackathlon-HA.o: .././Hackathlon-HA.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ .././I2C_master.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,3 +44,9 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+.././I2C_master.h:
--- Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
+++ Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
@@ -5,26 +5,37 @@
  *  Author: Ninja
  */ 
 
+#ifndef F_CPU
 #define F_CPU 16000000
+#endif
 
 #include <avr/io.h>
-#include "util/delay.h"
+#include <util/delay.h>
+#include <math.h>
+#include <stdlib.h>
+#include "I2C_master.h"
 
+#define DISPLAY_WRITE 0x4E 
+#define DISPLAY_READ 0xF
 
+void init_LCD(void){
 
-int main(void)
-{
-	uint8_t u8state = 0;
-	DDRB = (1<<3);
+	i2c_start(DISPLAY_WRITE);
+	i2c_write(0x60); // set pointer to CRA
+	i2c_write(0x88); // write 0x70 to CRA
+	i2c_stop();
+
+}
+
+int main(void){
 	
-    while(1) {
-        //TODO:: Please write your application code 
-		u8state ^= 0x01;
-		if (u8state) {
-			PORTB |= (1 << 2);
-		} else {
-			PORTB &= ~(1 << 2);
-		}
-		_delay_ms(500);
-    }
+	i2c_init();
+	init_LCD();
+	
+	while(1){
+
+		_delay_ms(1000);
+	}
+	
+	return 0;
 }
