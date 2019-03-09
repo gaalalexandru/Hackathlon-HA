d918e6c519ecccbfbc99ba6cd2d4b84ee09b09d6
florealucian <florea.razvanlucian@gmail.com>
2019-03-09 10:48:31 +0200

    I did it again

--- Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
+++ Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
@@ -15,15 +15,15 @@
 int main(void)
 {
 	uint8_t u8state = 0;
-	DDRB = (1<<5);
+	DDRB = (1<<3);
 	
     while(1) {
         //TODO:: Please write your application code 
 		u8state ^= 0x01;
 		if (u8state) {
-			PORTB |= (1 << 5);
+			PORTB |= (1 << 2);
 		} else {
-			PORTB &= ~(1 << 5);
+			PORTB &= ~(1 << 2);
 		}
 		_delay_ms(500);
     }
