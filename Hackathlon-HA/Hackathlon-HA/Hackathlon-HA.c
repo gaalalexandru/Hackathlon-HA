79fd5a5203a8a25444c534f038fcf3a94136ac3a
Gaal Alexandru <gsx_16@yahoo.com>
2019-03-09 10:03:58 +0200

    initial commit

---
+++ Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
@@ -0,0 +1,30 @@
+/*
+ * Hackathlon_HA.c
+ *
+ * Created: 3/9/2019 9:30:36 AM
+ *  Author: Ninja
+ */ 
+
+#define F_CPU 16000000
+
+#include <avr/io.h>
+#include "util/delay.h"
+
+
+
+int main(void)
+{
+	uint8_t u8state = 0;
+	DDRB = (1<<3);
+	
+    while(1) {
+        //TODO:: Please write your application code 
+		u8state ^= 0x01;
+		if (u8state) {
+			PORTB |= (1 << 3);
+		} else {
+			PORTB &= ~(1 << 3);
+		}
+		_delay_ms(500);
+    }
+}
