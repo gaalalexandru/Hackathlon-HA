b892fb785d8d4678ef8137b21ade4d6f50a63004
Gaal Alexandru <gsx_16@yahoo.com>
2019-03-09 10:07:54 +0200

    123

--- Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
+++ Hackathlon-HA/Hackathlon-HA/Hackathlon-HA.c
@@ -15,15 +15,15 @@
 int main(void)
 {
 	uint8_t u8state = 0;
-	DDRB = (1<<3);
+	DDRB = (1<<5);
 	
     while(1) {
         //TODO:: Please write your application code 
 		u8state ^= 0x01;
 		if (u8state) {
-			PORTB |= (1 << 3);
+			PORTB |= (1 << 5);
 		} else {
-			PORTB &= ~(1 << 3);
+			PORTB &= ~(1 << 5);
 		}
 		_delay_ms(500);
     }
